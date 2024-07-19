bb4022f439f341a0cfaf551761d5cbd91ddfc872
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-19 17:17:23 +0200

    initial files

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Chetan Won The Jackpot");
+
+
+
+// on button click add class="animationjs"
+// add .animate-rainbow clsas to color changing div
+// add animate-bounce class to h1 span element with id 
