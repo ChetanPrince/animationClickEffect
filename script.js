800d8e6be68112c817d91ab3f276aeb8d6df17fd
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-19 17:23:47 +0200

    added javascript to add classes and apply those animations

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ console.log("Chetan Won The Jackpot");
 // on button click add class="animationjs"
 // add .animate-rainbow clsas to color changing div
 // add animate-bounce class to h1 span element with id 
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", ()=>{
+    const moneyAnim = document.getElementById("emphasis");
+    const divColor = document.getElementById("color-changing");
+    const btnEffect = document.getElementById("btn");
+    moneyAnim.classList.add("animate-bounce");
+    divColor.classList.add("animate-rainbow");
+    btn.classList.add("animationjs");
+setTimeout(()=>{
+        moneyAnim.classList.remove("animate-bounce");
+        divColor.classList.remove("animate-rainbow");
+        btn.classList.remove("animationjs");
+
+    }, 3000);
+})
